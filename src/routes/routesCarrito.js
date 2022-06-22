287a7cbff5ed64ee7481640c363dded46cd81685
Gonzalo Huerta <gonzaloahuerta@gmail.com>
2022-06-22 01:16:32 -0300

    Primera entrega ok

---
+++ src/routes/routesCarrito.js
@@ -0,0 +1,86 @@
+import {Router} from 'express';
+import Api from '../apiClass.js';
+
+const cartApi = new Api('/db/carrito.json');
+const productsApi = new Api('/db/productos.json');
+const router = Router();
+
+//a
+router.post('/', async(req, res)=>{
+    const obj = req.body;
+    const {productos} = req.body
+    const productosAAgregar = [];
+    let existeProducto = true;
+
+    productos.forEach(async id => {
+        if(await productsApi.getById(id) == null){
+            existeProducto = false;
+            return;
+        }
+        productosAAgregar.push(await productsApi.getById(id));
+        obj.productos = productosAAgregar;
+    });
+    
+    setTimeout(async() => {
+        if(existeProducto){
+            const cart = await cartApi.create(obj);
+            res.json(cart);
+        }else{
+            res.json({error: 'Uno o más productos que intenta agregar al carrito no existen'})
+        }
+    }, 1000);
+    
+})
+
+//b
+router.delete('/:id', async(req, res)=>{
+    const {id} = req.params;
+    res.json(await cartApi.deleteById(id));
+})
+
+//c
+router.get('/:id/productos', async(req, res)=>{
+    const {id} = req.params;
+    const productsCart = await cartApi.getProductsCart(id);
+    res.json(productsCart);
+})
+
+//d
+router.post('/:id_cart/productos', async(req, res)=>{
+    const {id_cart} = req.params;
+    const {productos} = req.body
+    let productosAAgregar = [];
+    let error = false;
+    const cart = await cartApi.getById(id_cart);
+
+    if(cart==null){
+        res.json({error: 'No existe carrito con el id indicado'})
+        return;
+    }
+
+    productos.forEach(async id => {
+        if(await productsApi.getById(id)==null){
+            error = true;
+            return;
+        }
+        productosAAgregar.push(await productsApi.getById(id))
+    });
+
+    setTimeout(async() => {
+        if(!error){
+            const addProducts = await cartApi.addProductsToCart(productosAAgregar, id_cart);
+            res.json(addProducts);
+        }else{
+            res.json({error: 'Uno o más productos que intenta agregar al carrito no existen'})
+        }
+    }, 1000);
+})
+
+//e
+router.delete('/:id_cart/productos/:id_prod', async(req, res)=>{
+    const {id_cart, id_prod} = req.params;
+    const deleteProduct = await cartApi.deleteProductToCart(id_cart, id_prod);
+    res.json(deleteProduct);
+})
+
+export default router;
---
+++ src/routes/routesProductos.js
@@ -0,0 +1,64 @@
+import {Router} from 'express';
+import Api from '../apiClass.js';
+
+const api = new Api('/db/productos.json');
+const router = Router();
+
+const isAdmin = true;
+
+//middleware
+function checkIfIsAdmin(req, res, next){
+    if(!isAdmin){
+        res.json({error: "Acceso denegado. No tiene permisos para acceder a esta ruta"});
+    }else{
+        next();
+    }
+}
+
+router.get('/', async(req, res)=>{
+    const productos = await api.getAll();
+    res.json(productos);
+})
+
+//a
+router.get('/:id', async(req, res)=>{
+    const {id} = req.params;
+    const product = await api.getById(id);
+    res.json(product);
+})
+
+//b
+router.post('/', checkIfIsAdmin, async(req, res)=>{
+    const obj = req.body;
+    
+    if(obj.nombre==undefined || obj.descripcion==undefined || obj.codigo==undefined || 
+        obj.foto==undefined || obj.precio==undefined || obj.stock ==undefined)
+    {
+        res.json({error: "No se puede agregar un objeto vacío"})
+        return;
+    }
+    
+    for(let key in obj){
+        if(obj[key] == ''){
+            res.json({error: "Debe completar todos los campos para la creacion del producto"})
+            return;
+        }
+    }
+    const product = await api.create(obj);
+    res.json(product);
+})
+
+//c
+router.put('/:id', checkIfIsAdmin, async(req, res)=>{
+    const {id} = req.params;
+    const {timestamp, nombre, descripcion, codigo, foto, precio, stock} = req.body;
+    res.json(await api.editById(id, timestamp, nombre, descripcion, codigo, foto, precio, stock));
+})
+
+//d
+router.delete('/:id', checkIfIsAdmin, async(req, res)=>{
+    const {id} = req.params;
+    res.json(await api.deleteById(id));
+})
+
+export default router;
